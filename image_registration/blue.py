fc592ff3756edc9fd5f01dbcc3d52f1fa6be930f
Curtis <curtiscarljohnson@gmail.com>
2024-03-04 16:58:05 -0700

    getting decent images for answers

--- image_registration/blue.py
+++ image_registration/blue.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import glob
 
 
 def find_circles(image):
@@ -32,7 +33,7 @@ def count_circles_on_sides(circles):
     #top will have smallest y values, sort by y values, then see when y values changes too much
     circles_y = circles[0, :, 1]
     y = np.sort(circles_y)
-    print(y)
+    # print(y)
     min_y = y[0]
 
     #find first index where y value changes too much
@@ -47,7 +48,7 @@ def count_circles_on_sides(circles):
     #left will have smallest x values
     circles_x = circles[0, :, 0]
     x = np.sort(circles_x)
-    print(x)
+    # print(x)
     min_x = x[0]
 
     num_circles_on_left = np.argmax(x > min_x + threshold)
@@ -58,23 +59,48 @@ def count_circles_on_sides(circles):
     num_circles_on_right = len(x) - np.argmax(x > max_x - threshold)
     print(f"num_circles_on_right: {num_circles_on_right}")
 
+    return num_circles_on_top, num_circles_on_bottom, num_circles_on_left, num_circles_on_right
+
+
+def rotate_to_reference(image, numCirclesOnSides):
+    six_side = np.argmax(numCirclesOnSides)
+
+    if six_side == 0:
+        return image
+    elif six_side == 1:
+        return cv.rotate(image, cv.ROTATE_180)
+    elif six_side == 2:
+        return cv.rotate(image, cv.ROTATE_90_CLOCKWISE)
+    elif six_side == 3:
+        return cv.rotate(image, cv.ROTATE_90_COUNTERCLOCKWISE)
+
 
 def main():
-    ref = cv.imread('./images/Blue.jpg')
+    ref = cv.imread('./images/Blue.jpg', cv.IMREAD_GRAYSCALE)
     ref = cv.resize(ref, (0, 0), fx=0.25, fy=0.25)
-
-    blue = cv.imread('./images/Blue.jpg', cv.IMREAD_GRAYSCALE)
-    blue = cv.resize(blue, ref.shape[:2][::-1])
-    blue = cv.medianBlur(blue, 5)
-    blue = cv.threshold(blue, 50, 255, cv.THRESH_BINARY_INV)[1]
-
-    circles = find_circles(blue)
-    count_circles_on_sides(circles)
-    ref = draw_circles(ref, circles)
-
-    # cv.imshow('Blue Circles', blue)
     cv.imshow('Reference', ref)
-    cv.waitKey(0)
+    blue_images = glob.glob('./images/Blue *')
+
+    for blue_file in blue_images:
+        print(blue_file)
+        blue = cv.imread(blue_file, cv.IMREAD_GRAYSCALE)
+        blue = cv.resize(blue, (0, 0), fx=0.25, fy=0.25)
+        blue = cv.medianBlur(blue, 5)
+        cv.imshow("Raw Blue", blue)
+        blue = cv.threshold(blue, 150, 255, cv.THRESH_BINARY_INV)[1]
+        cv.imshow("Threshold Blue", blue)
+        circles = find_circles(blue)
+
+        numCircs = count_circles_on_sides(circles)
+        blue = rotate_to_reference(blue, numCircs)
+
+        blue = cv.resize(blue, ref.shape[:2][::-1])
+        diff = cv.absdiff(ref, blue)
+
+        cv.imshow('Blue Circles', blue)
+        cv.imshow('Diff', diff)
+        # cv.imshow('Reference', ref)
+        cv.waitKey(0)
 
 
 if __name__ == '__main__':
